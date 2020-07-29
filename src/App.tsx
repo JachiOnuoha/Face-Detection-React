251b3f21df8017e19de7ad3b25f746d694a3bf7a
Jachimike Onuoha <t-jaonuo@microsoft.com>
2020-07-28 22:08:41 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PrimaryButton} from 'office-ui-fabric-react';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  const [url, setUrl] = React.useState('');
+  const apiKey = '5f84b864125b47538872227cc200a482';
+  const endpoint = 'https://faceid-workshop.cognitiveservices.azure.com/face/v1.0/detect'
+  const params = `returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender`;
+  
+  const ImageAPI= (): Promise<void> | void => {
+    fetch (`${endpoint}?${params}`, {
+        method: 'POST',
+        headers: {
+          'Ocp-Apim-Subscription-Key': apiKey,
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({url: url}),
+   }
+    ).then(async (response: Response) =>{
+        if(response.ok){
+            const result:any = await response.json();
+            console.log(result)
+       }
+        else{
+           console.log(url);
+       }
+    });
+  
+  };
+
+  const updateUrl = (_: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | undefined): void => {
+  if (newValue !== undefined) {
+    setUrl(newValue);
+  }
+     };
+
+    // Add an onChange function to monitor the text field and work on making the onClick function call the API
+    return (
+      <div className='App'>
+       <title>Image-ine</title>
+      <div>
+          <h1>Welcome to Image-ine!</h1>
+           <p>The world's leading image analysis software developed by a guy name Jachi</p>
+        </div>
+        <div className='App-content'>
+        <form>
+        <TextField label="Please enter an image url" styles={{root:{marginTop: '10px'}}} onChange={updateUrl}/>
+        </form>
+      
+        <PrimaryButton text="Identify" styles={{root:{float: 'right', marginTop: '10px'}}} onClick={ImageAPI}/>
+       </div>
+      
+      </div>
+    );
 }
 
 export default App;
---
+++ src/CogService.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+// Consider moving this to the App.tsx folder 
